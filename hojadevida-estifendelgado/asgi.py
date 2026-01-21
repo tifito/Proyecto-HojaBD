7200848ca8a4de0cea8c7478bb7fce34e8be9c3d
tifito <tifitods@gmail.com>
2026-01-20 20:28:50 -0500

    Últimos cambios 2

--- hojadevida-estifendelgado/asgi.py
+++ hojadevida-estifendelgado/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for djangocrud project.
+ASGI config for hojadevida-estifendelgado project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djangocrud.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hojadevida-estifendelgado.settings')
 
 application = get_asgi_application()
